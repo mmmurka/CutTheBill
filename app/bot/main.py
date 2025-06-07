a0b93a1a2605fe860688f1e8b04e668bf1a3ae09
Nemsh <Artnemshilov@gmail.com>
2025-06-07 19:28:47 +0200

    bot echo, alembic

---
+++ app/bot/main.py
@@ -0,0 +1,21 @@
+from settings import TELEGRAM_BOT_TOKEN
+
+from aiogram import Bot, Router, Dispatcher
+
+bot = Bot(token=TELEGRAM_BOT_TOKEN)
+
+dp = Dispatcher()
+router = Router()
+
+@router.message()
+async def echo(message):
+    await message.answer(message.text)
+
+dp.include_router(router)
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
---
+++ app/database/database.py
@@ -0,0 +1,15 @@
+from contextlib import asynccontextmanager
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+from settings import DATABASE_URL
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+
+@asynccontextmanager
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
