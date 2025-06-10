6afdde503c88918583f473f2b6df45418e182ff5
Nemsh <Artnemshilov@gmail.com>
2025-06-10 17:09:06 +0200

    alembic new migration, admin, user DAL

--- src/database/models.py
+++ src/database/models.py
@@ -9,7 +9,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    telegram_id = Column(Integer, unique=True)
+    telegram_id = Column(Integer, unique=False)
     username = Column(String, unique=True, nullable=True)
 
     linked_services = relationship("LinkedService", back_populates="user", cascade="all, delete")
@@ -21,8 +21,8 @@ class Service(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True)
-    subscription_price = Column(Float)
-    user_price = Column(Float)
+    subscription_price = Column(Float, nullable=False)
+    user_price = Column(Float, nullable=False)
 
     linked_services = relationship("LinkedService", back_populates="service", cascade="all, delete")
 
@@ -31,8 +31,8 @@ class LinkedService(Base):
     __tablename__ = "linked_services"
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(ForeignKey("users.id"))
-    service_id = Column(ForeignKey("services.id"))
+    user_id = Column(ForeignKey("users.id"), nullable=False)
+    service_id = Column(ForeignKey("services.id"), nullable=False)
 
     user = relationship("User", back_populates="linked_services")
     service = relationship("Service", back_populates="linked_services")
@@ -43,7 +43,7 @@ class Group(Base):
     __tablename__ = "groups"
 
     id = Column(Integer, primary_key=True)
-    group_name = Column(String, unique=True)
+    group_name = Column(String, unique=False)
     service_id = Column(ForeignKey("services.id"), nullable=False)
     max_slots = Column(Integer, nullable=False)
     free_slots = Column(Integer, nullable=False)
@@ -56,9 +56,9 @@ class GroupUser(Base):
     __tablename__ = "group_users"
 
     id = Column(Integer, primary_key=True)
-    group_id = Column(ForeignKey("groups.id"))
-    user_id = Column(ForeignKey("users.id"))
-    email = Column(String)
+    group_id = Column(ForeignKey("groups.id"), nullable=False)
+    user_id = Column(ForeignKey("users.id"), nullable=False)
+    email = Column(String, nullable=False)
 
     group = relationship("Group", back_populates="users_assoc")
     user = relationship("User", back_populates="groups_assoc")
@@ -67,27 +67,28 @@ class Admin(Base):
     __tablename__ = "admins"
 
     id = Column(Integer, primary_key=True)
-    telegram_id = Column(Integer, unique=True)
+    telegram_id = Column(Integer, unique=True, nullable=False)
 
 class Payment(Base):
     __tablename__ = "payments"
 
     id = Column(Integer, primary_key=True)
-    linked_service_id = Column(ForeignKey("linked_services.id"))
-    amount = Column(Float)
+    linked_service_id = Column(ForeignKey("linked_services.id"), nullable=False)
+    amount = Column(Float, nullable=False)
 
     last_payment = Column(DateTime)
     next_payment = Column(DateTime)
 
     linked_service = relationship("LinkedService", back_populates="payments")
-    payment_logs = relationship("PaymentLog", back_populates="linked_service", cascade="all, delete")
+    payment_logs = relationship("PaymentLog", back_populates="payment", cascade="all, delete-orphan")
 
 class PaymentLog(Base):
     __tablename__ = "payment_logs"
 
     id = Column(Integer, primary_key=True)
-    linked_service_id = Column(ForeignKey("linked_services.id"))
-    amount = Column(Float)
+    payment_id = Column(ForeignKey("payments.id"), nullable=False)
+    amount = Column(Float, nullable=False)
     paid_at = Column(DateTime)
 
-    linked_service = relationship("LinkedService", back_populates="payment_logs")
+    payment = relationship("Payment", back_populates="payment_logs")
+
